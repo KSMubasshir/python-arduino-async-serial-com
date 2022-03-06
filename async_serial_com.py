0e9c471653ca10e414852b6f390f12071af38e7e
KSMubasshir <ksmubasshir@gmail.com>
2022-03-06 14:03:50 +0600

    v1 completion checkpoint

--- async_serial_com.py
+++ async_serial_com.py
@@ -5,6 +5,32 @@ import serial  # get from http://pyserial.sourceforge.net/
 import sys
 import time
 import threading
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+import tornado.gen
+from tornado.options import define, options
+import json
+from queue import Queue
+
+define("port", default=8080, help="run on the given port", type=int)
+
+clients = []
+
+input_queue = Queue()
+output_queue = Queue()
+
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+
+class StaticFileHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('static/main.js')
 
 
 class GetHWPoller(threading.Thread):
@@ -27,7 +53,7 @@ class GetHWPoller(threading.Thread):
         self.worker()
 
     def worker(self):
-        while (1):
+        while True:
             if self.runflag.is_set():
                 self.pollfunc()
                 time.sleep(self.sleeptime)
@@ -41,11 +67,11 @@ class GetHWPoller(threading.Thread):
         self.runflag.set()
 
     def running(self):
-        return (self.runflag.is_set())
+        return self.runflag.is_set()
 
 
-class HW_Interface(object):
-    """Class to interface with asynchrounous serial hardware.
+class HWInterface(object):
+    """Class to interface with asynchronous serial hardware.
   Repeatedly polls hardware, unless we are sending a command
   "ser" is a serial port class from the serial module """
 
@@ -62,7 +88,6 @@ class HW_Interface(object):
     def register_callback(self, proc):
         """Call this function when the hardware sends us serial data"""
         self.callback = proc
-        # self.callback("test!")
 
     def kill(self):
         self.worker.kill()
@@ -91,11 +116,61 @@ class HW_Interface(object):
                 return "OK"
         return "None"  # got no response
 
+portname = "/dev/ttyACM0"
+portbaud = "9600"
+ser = serial.Serial(portname, portbaud, timeout=0, write_timeout=0)
+hw = HWInterface(ser, 0.1)
+
 
 def my_callback(response):
     """example callback function to use with HW_interface class.
      Called when the target sends a byte, just print it out"""
     print('got HW response "%s"' % response)
+
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print('new connection')
+        clients.append(self)
+        self.write_message("connected")
+
+    def on_message(self, message):
+        print('tornado received from client: %s' % json.dumps(message))
+        # self.write_message('ack')
+        input_queue.put(message)
+
+    def on_close(self):
+        print('connection closed')
+        clients.remove(self)
+
+
+# check the queue for pending messages, and rely on that to all connected clients
+def check_queue():
+    # print("Checking Queue")
+    # print(input_queue.get())
+    if not input_queue.empty():
+        cmd = input_queue.get().split(' ')
+        if cmd[0] == 'r':
+            print('Last response: "%s"' % hw.response)
+            output_queue.put(hw.response)
+
+        elif cmd[0] == 's':
+            val = bytes(cmd[1][0], 'utf-8')
+            print("sending command " + val.decode('utf-8'))
+            sys.stdout.flush()
+            hw.write_HW(val)
+
+        elif cmd[0] == 'x':
+            print("exiting...")
+            exit()
+
+        else:
+            print("No such command: " + ' '.join(cmd))
+    if not output_queue.empty():
+        message = output_queue.get()
+        print(message)
+        for c in clients:
+            c.write_message(message)
 
 
 """Designed to talk to Arduino running very simple demo program. 
@@ -110,19 +185,29 @@ if __name__ == '__main__':
 
     # Need to set the portname for your Arduino serial port:
     # see "Serial Port" entry in the Arduino "Tools" menu
-    portname = "/dev/ttyACM0"
-    portbaud = "9600"
-    ser = serial.Serial(portname, portbaud, timeout=0, write_timeout=0)
+
+
     # timeout=0 means "non-blocking," ser.read() will always return, but
     # may return a null string.
     print("opened port " + portname + " at " + str(portbaud) + " baud")
 
     sys.stdout.flush()
-    hw = HW_Interface(ser, 0.1)
+
 
     # when class gets data from the Arduino, it will call the my_callback function
     hw.register_callback(my_callback)
 
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        handlers=[
+            (r"/", IndexHandler),
+            (r"/static/(.*)", tornado.web.StaticFileHandler, {'path': './'}),
+            (r"/ws", WebSocketHandler)
+        ]
+    )
+    httpServer = tornado.httpserver.HTTPServer(app)
+    httpServer.listen(options.port)
+    print("Listening on port:", options.port)
     print("Interactive mode. Commands:")
     print("  r to get last response byte")
     print("  s <str> to send string <str>")
@@ -130,23 +215,13 @@ if __name__ == '__main__':
     print("    's 0' turns LED off")
     print("  x to exit ")
 
-    while (1):
-        sys.stdout.flush()
-        cmd = input('--> ')
-        cmd = cmd.split()
-        sys.stdout.flush()
-        if cmd[0] == 'r':
-            print('Last response: "%s"' % hw.response)
+    mainLoop = tornado.ioloop.IOLoop.instance()
+    # adjust the scheduler_interval according to the frames sent by the serial port
+    scheduler_interval = 100
+    scheduler = tornado.ioloop.PeriodicCallback(check_queue, scheduler_interval)
+    scheduler.start()
+    mainLoop.start()
 
-        elif cmd[0] == 's':
-            val = bytes(cmd[1][0], 'utf-8')
-            print("sending command " + val.decode('utf-8'))
-            sys.stdout.flush()
-            hw.write_HW(val)
 
-        elif cmd[0] == 'x':
-            print("exiting...")
-            exit()
 
-        else:
-            print("No such command: " + ' '.join(cmd))
+
